d847f6efc86850c36800fd1d8863508cf4a9b05c
SachinSharma22 <s830sharma@gmail.com>
2025-04-06 01:09:57 +0530

    Math in Javascript

---
+++ 02_Math/app.js
@@ -0,0 +1,23 @@
+console.log(Math.PI);
+
+console.log(Math.sqrt(4));
+
+console.log(Math.pow(2, 5));
+
+console.log(2 ** 5);
+
+console.log(Math.floor(2.345345));
+
+console.log(Math.floor(-3.33));
+
+console.log(Math.ceil(3.33));
+
+console.log(Math.ceil(-3.33));
+
+console.log(Math.round(3.3));
+console.log(Math.round(3.6));
+console.log(Math.round(3.4999999999));
+
+// console.log(Math.random()); 
+
+// console.log(Math.floor(Math.random() * 20));
