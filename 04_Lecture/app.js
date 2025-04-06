777e0133df8dad6b00e8cd71c64dc20eb3721a5c
SachinSharma22 <s830sharma@gmail.com>
2025-04-06 10:42:01 +0530

    Comparison Operators

---
+++ 04_Lecture/app.js
@@ -0,0 +1,11 @@
+let one = "33";
+let two = 33;
+console.log(one == two); //Here implicit conversion is performed
+console.log(one === two);
+
+console.log(one > two);
+console.log(one < two);
+console.log(one <= two);
+console.log(one >= two);
+ console.log(one != two);
+ console.log(one !==two);
