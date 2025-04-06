9af54bd6ba98da8b3cf2377f2583e01a437ae251
SachinSharma22 <s830sharma@gmail.com>
2025-04-06 12:15:10 +0530

    decision Making in JavaScript

---
+++ 06_Lecture/app.js
@@ -0,0 +1,35 @@
+const username = "sachin";
+const userAge = 23;
+// if(true) console.log("user is a working professional");
+
+if(userAge >= 18) console.log("User is a working professional");
+
+console.log("Program Ended");
+
+
+let num1 = 23;
+if(num1 < 18) {
+    console.log("You can't drive. ");
+}else if(num1 >=18){
+    console.log("You can drive");
+}
+
+let day = "Sunday";
+
+if(day === "Sunday"){
+    console.log("Today is Sunday");
+}else if(day === "Monday"){
+    console.log("Today is Monday");
+}else if(day === "Tuesday"){
+    console.log("Today is Tuesday");
+}else if(day === "Wednesday"){
+    console.log("Today is Wednesday");
+}else if(day === "Thursday"){
+    console.log("Today is Thursday");
+}else if(day === "Friday"){
+    console.log("Today is Friday");
+}else if(day === "Saturday"){
+    console.log("Today is Saturday");
+}else{
+    console.log("Enter a valid day Name");
+}
