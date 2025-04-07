a53b1f7f1e8f65af1b1475bc9ac1da5ed6f6396d
SachinSharma22 <s830sharma@gmail.com>
2025-04-07 15:11:58 +0530

    Combined Assignment Operator

---
+++ 13_Lecture/app.js
@@ -0,0 +1,33 @@
+// let num = 5;
+// num += 5;
+// console.log(num);
+
+// let num = 15;
+// num -= 5;
+// console.log(num);
+
+// let num = 5;
+// num *= 5;
+// console.log(num);
+
+// let num = 5;
+// num /= 5;
+// console.log(num);
+
+// let num = 16;
+// num %= 5;
+// console.log(num);
+
+// let num = 5;
+// num **= 5;
+// console.log(num);
+
+// console.log(5 ** 5);
+
+// let num = 5;
+// num++;
+// console.log(num);
+
+num = 5;
+num++ + ++num;
+console.log(num)
