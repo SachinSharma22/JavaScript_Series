567851055db02cfa7a78df30a083adcd4b67eee8
SachinSharma22 <s830sharma@gmail.com>
2025-04-07 13:36:50 +0530

    Methods in Array

---
+++ 11_lecture/app.js
@@ -0,0 +1,41 @@
+const coll = ["papaya", "apple", "banana", "grapes","orange"];
+
+// console.log(coll.includes("banana"));
+
+// console.log(coll.length);
+// console.log(coll.push("papaya"));
+
+
+// const newArr = {
+//     name: "sachin",
+//     age: 23
+// };
+// coll.push(newArr);
+// console.log(coll[5].name);
+// console.log(coll.pop());
+
+// let newSlice = coll.slice(1,3);
+// console.log(newSlice);
+
+// const reverseArr = coll.reverse();
+// console.log(reverseArr);
+
+// coll.unshift("iphone");
+// console.log(coll);
+
+// console.log(coll.shift());
+// console.log(coll);
+
+// let newAr = ['a', 'b'];
+
+// console.log(coll.concat(newAr));
+
+// console.log(coll.indexOf("banana"));
+
+// console.log(coll.sort());
+
+console.log(coll.splice(2,0 , "iphone", "redmi"));
+console.log(coll);
+
+
+
