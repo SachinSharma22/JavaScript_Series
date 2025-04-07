bd6ab341bfed193b1798a9a985c415e45f6221e2
SachinSharma22 <s830sharma@gmail.com>
2025-04-07 10:59:21 +0530

    Switch case in JavaScript

---
+++ 07_Lecture/app.js
@@ -0,0 +1,27 @@
+let num = 2;
+
+
+
+switch(num) {
+    case 1:
+        console.log("This is One");
+    break;
+    case 2:
+        console.log("This is two");
+    break;
+    case 3:
+        console.log("This is Three");
+    break;
+    case 4:
+        console.log("This is four");
+    break;
+    case 5:
+        console.log("This is Five");
+    break;
+    case 6:
+        console.log("This is six");
+    break;
+    default:
+        console.log("Enter a valid number");
+    break;
+}
