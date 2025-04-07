8ab54d437b1d50d2e8df2f0dc2a64b9502c57eb5
SachinSharma22 <s830sharma@gmail.com>
2025-04-07 18:21:36 +0530

    functions in JavaScript

---
+++ 17_Lecture/script.js
@@ -0,0 +1,48 @@
+// function greetMe() {
+//     console.log("Hello Sachin!");
+// }
+
+// greetMe();
+
+// let greet = function() {
+//     console.log("Again Hello Sachin!");
+// }
+
+// greet();
+
+// let addition = function(a,b) {
+//        return a + b;
+//     }
+
+// console.log(addition(1,2));
+    
+
+// function greetMe(value = 'Saurabh', prof){
+//     console.log(`Good morning: ${value}`);
+//     console.log(`I am a ${prof}`);
+// }
+
+// // greetMe("sachin", "frontend Developer");
+// // greetMe("Pawan");
+
+// greetMe();
+
+
+function addTwoNumber(num1, num2){
+    return num1 + num2;
+}
+
+console.log(addTwoNumber(addTwoNumber(5,6), addTwoNumber(7,6)));
+
+//This is clouser in JavaScript
+// function jodo(num1, num2){
+//     // return num1 + num2;
+//     function sum() {
+//         return num1 + num2;
+//     }
+
+//     return sum();
+// }
+
+// let result = jodo(5, 6);
+// console.log(result);
