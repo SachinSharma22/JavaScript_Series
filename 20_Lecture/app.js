df9d7e21970e3ffc52d42ca2a356b88b0fa005a1
SachinSharma22 <s830sharma@gmail.com>
2025-04-09 15:05:08 +0530

    setTimeout() and setInterval()

---
+++ 20_Lecture/app.js
@@ -0,0 +1,11 @@
+setTimeout(() => {
+    console.log("This will run after 2 second");
+}, 2000);
+
+// setInterval(() => {
+//     console.log("This will run every 2 second");
+// }, 2000);
+
+// setInterval(() => {
+//     console.log("This will run every 3 second");
+// }, 3000);
