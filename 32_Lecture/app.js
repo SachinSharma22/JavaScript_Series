b14abba98c228de967db83b6b45c3a2cd7d61580
SachinSharma22 <s830sharma@gmail.com>
2025-04-14 15:27:09 +0530

    destructuring

---
+++ 32_Lecture/app.js
@@ -0,0 +1,37 @@
+const colors = ["red", 'green', 'blue'];
+
+const [first, second] = colors;
+console.log(first);
+console.log(second);
+
+
+// You can even skip some items or use rest
+
+const  [first1 , , third] = colors;
+
+console.log(first1);
+console.log(third);
+
+//YOu can also use rest parameter to use other data
+
+const [first2, ...other] = colors;
+
+console.log(first2);
+console.log(other);
+
+const users = {
+    name: "sachin",
+    age: 23
+};
+
+const {name, age} = users;
+
+console.log(name);
+console.log(age);
+
+function userDetail({name, age}) {
+    console.log(name);
+    console.log(age);
+}
+
+userDetail(users)
