91d1247609018c45d152e05e1e87ed949aa081b1
SachinSharma22 <s830sharma@gmail.com>
2025-08-30 01:49:04 +0530

    another folder

---
+++ Day1/script.js
@@ -0,0 +1,15 @@
+// const myName = "Sachin"
+// document.writeln(`<h1 id="first">I am ${myName} </h1><br>`);
+// document.writeln(`${myName} is a developer <br>`);
+// document.writeln(`${myName} loves coding <br>`);
+// document.writeln(`${myName} also reads the books <br>`);
+// document.writeln(`${myName} cooks really good food <br>`)
+
+// const firstClass = document.getElementById("first");
+
+// firstClass.addEventListener('click', () => {
+//     setTimeout(() => {
+//         alert("This is JS_Workshop")
+//     },1500)
+// })
+
